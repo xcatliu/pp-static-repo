280ee39921af99d54b324967b793c63dc66d1901
pengshanglong <pengshanglong@simuwang.com>
2019-11-04 19:07:34 +0800

    first commit

---
+++ app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const msg = 'this is a none use message!'
+
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <header>
+        <h1 className="App-header">hello world!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
